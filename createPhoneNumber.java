d573f66936e7990257938f65bdb6a8107dcbd55e
Bruno Cestari <bccestari.project@gmail.com>
2024-06-17 20:43:13 -0300

    Write a fuction that take a int array andreturn phone number string

---
+++ createPhoneNumber.java
@@ -0,0 +1,31 @@
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!
+
+
+*/
+
+//Solution
+
+
+public class Kata {
+    public static String createPhoneNumber(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
+        for (int i =0 ; i < numbers.length; i++){
+            sb.append(numbers[i]);
+        }
+        sb.insert(0, "(");
+        sb.insert(4, ")");
+        sb.insert(5, " ");
+        sb.insert(9, "-");
+
+        return sb.toString();
+    }
+}
+
+
