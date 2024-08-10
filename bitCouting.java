46b2a54ddd21f6a486ef2e816e84759c668f422f
Bruno Cestari <bccestari.project@gmail.com>
2024-08-10 18:22:50 -0300

    Implement function to count the number of 1 bits in the binary representation of a integer

---
+++ bitCouting.java
@@ -0,0 +1,33 @@
+/*
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+*/
+
+//Solution
+
+
+public class BitCounting {
+
+    public static int countBits(int n){
+        // Show me the code!
+        String binaryString = Integer.toBinaryString(n);
+        int sum = 0;
+        for (int i = 0; i < binaryString.length(); i++){
+            char ch = binaryString.charAt(i);
+            int numericValue = Character.getNumericValue(ch);
+            sum += numericValue;
+        }
+        return sum;
+    }
+
+// Cool solution from another user:
+
+    public static int countBitsCool(int n) {
+        return Integer.toBinaryString(n).replaceAll("0","").length();
+    }
+}
+
+
+
