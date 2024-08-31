364c43222934fdaaac3caac9f2b5a914834ee17a
Bruno Cestari <bccestari.project@gmail.com>
2024-08-31 18:31:37 -0300

    Implement method to convert seconds to human-readable HH:MM:SS format

---
+++ humanReadableTime.java
@@ -0,0 +1,27 @@
+/*
+https://www.codewars.com/kata/52685f7382004e774f0001f7/java
+
+DESCRIPTION:
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+*/
+
+//Solution
+
+
+import javax.swing.*;
+
+public class HumanReadableTime {
+    public static String makeReadable(int seconds) {
+        int h = seconds/60/60;
+        int min = seconds/60%60;
+        int sec = seconds%60;
+        return String.format("%02d:%02d:%02d",h,min,sec);
+    }
+}
