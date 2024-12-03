3e113ed0b6866ba0aaad59547c42675a4a76d8e4
Bruno Cestari <bccestari.project@gmail.com>
2024-12-02 23:58:12 -0300

    Implement outlier detection method to find the single odd or even number in an array

--- FindOutlier.java
+++ FindOutlier.java
@@ -1,4 +1,4 @@
-/*
+    /*
 https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/java
 
 You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
@@ -37,3 +37,26 @@ public class FindOutlier {
         return 0; // no outlier found
     }
 }
+
+
+
+
+//Clean solution:
+
+class FindOutlier2 {
+        static int find(int[] integers) {
+
+            boolean isMostlyEven =
+                    (integers[0] % 2 == 0 ? 1 : 0) +
+                            (integers[1] % 2 == 0 ? 1 : 0) +
+                            (integers[2] % 2 == 0 ? 1 : 0) >= 2;
+
+            for (int number : integers) {
+                if ((isMostlyEven && number % 2 != 0) || (!isMostlyEven && number % 2 == 0)) {
+                    return number;
+                }
+            }
+
+            return 0;
+        }
+    }
