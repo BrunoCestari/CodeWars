668e5a9e1e4ed3b0da03360392c1b70a9a3c8f4d
Bruno Cestari <bccestari.project@gmail.com>
2025-01-04 19:17:05 -0300

    refactor: implement string splitting into pairs with underscore for odd-length strings

--- SplitStrings.java
+++ SplitStrings.java
@@ -11,35 +11,55 @@ Examples:
 */
 
 
-//Solution
-
+//REFACTORED SOLUTION
 
 public class StringSplit {
     public static String[] solution(String s) {
+        int arrayLength = (s.length() + 1) / 2;
+        String[] result = new String[arrayLength];
 
-        char[] arr = s.toCharArray();
-
-
-        // Even string
-        if (s.length() % 2 == 0) {
-            String[] solutionArray = new String[s.length() / 2];
-            for (int i = 0; i < solutionArray.length; i++) {
-                StringBuilder sb = new StringBuilder();
-                solutionArray[i] = sb.append(arr[i*2]).append(arr[i*2+1]).toString();
-            }
-            return solutionArray;
+        for (int i = 0; i < s.length(); i += 2) {
+            // Get the first character
+            char first = s.charAt(i);
+            char second = (i + 1 < s.length()) ? s.charAt(i + 1) : '_';
+            result[i / 2] = "" + first + second;
         }
-
-        // Odd String
-        String[] solutionArray = new String[(s.length() + 1 )/ 2];
-        for (int i = 0; i < solutionArray.length -1; i++) {
-            StringBuilder sb = new StringBuilder();
-            solutionArray[i] = sb.append(arr[i*2]).append(arr[i*2 +1]).toString();
-        }
-
-        StringBuilder sb = new StringBuilder();
-        solutionArray[solutionArray.length -1] = sb.append(arr[arr.length -1]).append("_").toString();
-        return solutionArray;
+        return result;
     }
-
 }
+
+
+
+
+
+//FIRST SOLUTION
+
+//public class StringSplit {
+//    public static String[] solution(String s) {
+//
+//        char[] arr = s.toCharArray();
+//
+//
+//        // Even string
+//        if (s.length() % 2 == 0) {
+//            String[] solutionArray = new String[s.length() / 2];
+//            for (int i = 0; i < solutionArray.length; i++) {
+//                StringBuilder sb = new StringBuilder();
+//                solutionArray[i] = sb.append(arr[i*2]).append(arr[i*2+1]).toString();
+//            }
+//            return solutionArray;
+//        }
+//
+//        // Odd String
+//        String[] solutionArray = new String[(s.length() + 1 )/ 2];
+//        for (int i = 0; i < solutionArray.length -1; i++) {
+//            StringBuilder sb = new StringBuilder();
+//            solutionArray[i] = sb.append(arr[i*2]).append(arr[i*2 +1]).toString();
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//        solutionArray[solutionArray.length -1] = sb.append(arr[arr.length -1]).append("_").toString();
+//        return solutionArray;
+//    }
+//
+//}
